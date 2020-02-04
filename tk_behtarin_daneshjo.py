feeb5dbdf82ff9fc3be70f35330ce228cb261956
mahdi7fallah <60651462+mahdi7fallah@users.noreply.github.com>
2020-02-04 17:15:08 +0330

    Create tk_behtarin_daneshjo.py

---
+++ tk_behtarin_daneshjo.py
@@ -0,0 +1,27 @@
+# دریافت نام و نمره تعدادی دانشجو
+# نشان دادن دانشجویی که بالاترین نمره را دارد
+
+#pylint:disable=C0103
+
+tedad = int(input("Tedad DaneshjoyHa: "))
+
+daneshjo_nomre = []     # Tedad Kole DaneshjoHa Ba Nomre
+d_name = []                  # Name DaneshjoHa
+d_nomre = []                 # Nomre DaneshjoHa
+
+for i in range(tedad):
+    d_name += [input("\nName Daneshjo {}: ".format(i + 1))]
+    daneshjo_nomre += [d_name[i]]
+    d_nomre += [input("Nomre {}: ".format(d_name[i]))]
+    daneshjo_nomre += [d_nomre[i]]
+    
+max_nomre = max(d_nomre)                                   # Pehda Kardan Behtarin Nomre
+index_max_nomre = d_nomre.index(max_nomre)     # Index Behtarin Nomre
+behtarin_daneshjo = d_name[index_max_nomre]      # Behtarin Daneshjo
+
+print("\n", daneshjo_nomre)
+print("\nName  DaneshjoHa: ", d_name)
+print("Nomre DaneshjoHa: ", d_nomre)
+print("\nBehtarin Nomre: ", max_nomre)
+print("Shamoreye Behtarin Nomre(Daneshjo): ", index_max_nomre + 1)
+print("Behtarin Daneshjo: ", behtarin_daneshjo)
